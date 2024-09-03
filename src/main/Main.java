a0981e6c90a3e6ea86a3c87d36cf3a7c8d0b4b09
JhonP16 <jhonpulgarinrestrepo@gmail.com>
2024-09-03 17:28:20 -0500

    Actualización del archivo Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola :) Somos un equipo de PDS, este es el ejemplo del Main");
+        System.out.println("Este es el programa principal");
 
     }
 }
